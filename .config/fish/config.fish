1145ba818d63a20480650950429e8fb944436cec
dotnetx86 <dotnetx86@gmail.com>
2025-09-26 23:54:51 +0300

    welly

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+set -gx EDITOR vim
+
+alias rf trash
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
--- .config/hypr/volume.sh
+++ .config/hypr/volume.sh
@@ -19,14 +19,3 @@ elif [ "$1" = "up" ]; then
 else
   downcmd
 fi
-
-volume="$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100}')"
-mute="$(pactl get-sink-mute 0 | awk '{print $2}')"
-
-if [ "$mute" = "yes" ]; then
-  # Show the sound muted notification
-  notify-send -a "wp-vol" "Volume muted" --expire-time=1000
-else
-  # Show the volume notification
-  notify-send -a "wp-vol" -h int:value:"$volume" "Volume: ${volume}" --expire-time=1000
-fi
