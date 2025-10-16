0aa5658c7a0ad806070e46f1178e55e2db0b4e97
dotnetx86 <dotnetx86@gmail.com>
2025-10-16 13:28:25 +0300

    bye

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,15 @@ set -gx EDITOR vim
 
 alias rf trash
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
